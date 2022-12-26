88b45785c519d51be3400111b10f0d021cf1873c
Gotam Gorabh <gautamy672@gmail.com>
2022-12-26 20:53:23 +0530

    Day 13.0 : Adding variable.sh file

---
+++ code/variable.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# This is a comment.
+echo "Hello, World"
+
+# following are some system variables.
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# following is user-defined variable
+name=Gotam
+# 10val -> This is not a valid variable
+echo My name is $name
